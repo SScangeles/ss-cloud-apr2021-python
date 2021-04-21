7551f3da0f2ccb06fc1750d6c5df93c47689fda8
christianangeles <christian.angeles@smoothstack.com>
2021-04-21 12:56:09 -0700

    update code exercises

--- coding-exercise2.py
+++ coding-exercise2.py
@@ -1,8 +1,22 @@
 
-# numbers
+## 1) Numbers
+
 num_ex1 = 10 + 10
 num_ex2 = 5 - 1
 
-print(num_ex1)
-print(num_ex2)
-print(num_ex1+num_ex2)
+print(num_ex1, num_ex2, num_ex1 + num_ex2)
+
+## 2) Output for: 30+*6, 6^6, 6**6, 6+6+6+6+6+6
+
+# out_ex1 = 30+*6
+out_ex2 = 6^6
+out_ex3 = 6**6
+out_ex4 = 6+6+6+6+6+6
+
+print(out_ex2, out_ex3, out_ex4)
+
+## 3) "Hello World"
+
+print("Hello World", "\nHello World : 10")
+
+## 4) Mortgage Calc
