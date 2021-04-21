6d06496a73f906482ab99ab5cb9767e60d7660a0
christianangeles <christian.angeles@smoothstack.com>
2021-04-21 13:25:30 -0700

    update mortgage calc

--- coding-exercise2.py
+++ coding-exercise2.py
@@ -17,6 +17,20 @@ print(out_ex2, out_ex3, out_ex4)
 
 ## 3) "Hello World"
 
-print("Hello World", "\nHello World : 10")
+print("Hello World", "\nHello World :", 10)
 
 ## 4) Mortgage Calc
+
+principle = 800000
+rate = 0.06
+monthly_payments = 10000
+months = 103
+
+for x in range(months-1):
+    principle += (principle * (rate/12)) - monthly_payments
+
+last_payment = principle + (principle * (rate/12))
+
+payout = (months - 1) * monthly_payments + last_payment
+
+print("Mortgage payout: ", payout)
