566ece18838776b836e784b5e10cb335565e128f
christianangeles <christian.angeles@smoothstack.com>
2021-04-22 11:38:10 -0700

    update exercise3

--- coding-exercise3.py
+++ coding-exercise3.py
@@ -1,19 +1,27 @@
 
-## 1) Print 'r' from 'Hello World'
+# 1) Print 'r' from 'Hello World'
 print("Hello World"[8])
 
-## 2) String slicing
+# 2) String slicing
 print("thinker"[2:-2])
 
-## 3) Sammy
+# 3) Sammy
 s = "Sammy"
 print(s[2:])
 
-## 4) Mississippi to distinct character word
+# 4) Mississippi to distinct character word
 # print("Mississippi"[:])
 
-## 5) Palindrome
-def palindrome(input):
-    return print(input)
+# 5) Palindrome
 
-palindrome("stars")
+
+def palindrome(input):
+    result = input
+    if len(input) > 1:
+        result = "".join(("".join([x for x in input.split(",")])).split())
+        if result[::-1] == result:
+         return print("'{}' is palindrome:".format(input), result)
+    return print("not palindrome:", result)
+
+
+palindrome("h, an, nah")
