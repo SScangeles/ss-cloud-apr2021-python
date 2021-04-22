b1f6115338bc21f256a5d27084a6de3726dd7489
christianangeles <christian.angeles@smoothstack.com>
2021-04-22 13:15:40 -0700

    update exercise3

--- coding-exercise3.py
+++ coding-exercise3.py
@@ -14,13 +14,19 @@ print("".join(set("Mississippi")))
 
 import string
 def palindrome(input):
-    result = input
+    result = []
     exclude = set(string.punctuation)
-    if len(input) > 1:
-        result = "".join("".join(x for x in input if x not in exclude).split()).lower()
-        if result[::-1] == result:
-            return print("'{}' is a palindrome:".format(input), result)
-    return print("not a palindrome:", result)
+    print("Panlindrome check:\n{}".format(input))
+    for word in input:
+        temp = word
+        if len(word) > 1:
+            temp = "".join("".join(x for x in word if x not in exclude).split()).lower()
+            if temp[::-1] == temp:
+                result.append("Y")
+            else:
+                result.append("N")
+        
+    return print(result)
 
 # 5) Palindrome ignore punctuations, single characters
-palindrome("H, an, nah!!")
+palindrome(["H, an, nah!!", "def not a palin"])
