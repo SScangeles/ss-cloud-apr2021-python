6d027ef8006545dccd48be5cfda81b3dec8475a0
christianangeles <christian.angeles@smoothstack.com>
2021-04-22 11:11:17 -0700

    init exercise 3 and 4

---
+++ coding-exercise3.py
@@ -0,0 +1,19 @@
+
+## 1) Print 'r' from 'Hello World'
+print("Hello World"[8])
+
+## 2) String slicing
+print("thinker"[2:-2])
+
+## 3) Sammy
+s = "Sammy"
+print(s[2:])
+
+## 4) Mississippi to distinct character word
+# print("Mississippi"[:])
+
+## 5) Palindrome
+def palindrome(input):
+    return print(input)
+
+palindrome("stars")
