daf997cb63e830f3c995db4ddcf8658029c2210b
christianangeles <christian.angeles@smoothstack.com>
2021-04-22 14:06:29 -0700

    update exercises

--- coding-exercise3.py
+++ coding-exercise3.py
@@ -14,10 +14,17 @@ print("".join(set("Mississippi")))
 
 import string
 def palindrome(input):
-    result = []
     exclude = set(string.punctuation)
+    result = []
+    temp_list = []
+
+    if isinstance(input, list):
+        temp_list = input
+    else:
+        temp_list.append(input)
+        
     print("Panlindrome check:\n{}".format(input))
-    for word in input:
+    for word in temp_list:
         temp = word
         if len(word) > 1:
             temp = "".join("".join(x for x in word if x not in exclude).split()).lower()
@@ -29,4 +36,4 @@ def palindrome(input):
     return print(result)
 
 # 5) Palindrome ignore punctuations, single characters
-palindrome(["H, an, nah!!", "def not a palin"])
+palindrome("H, an, nah!!")
---
+++ coding-exercise4.py
@@ -0,0 +1,27 @@
+
+# Question 1:
+# 1) List of ...
+list1 = [1, "hello world", 3.14]
+print(list1)
+
+# 2) List of [1,1[1,2]], grab 2
+list2 = [1, 1, [1,2]]
+print(list2[2][1])
+
+# 3) print list[1:]
+list3 = ["a", "b", "c"]
+print(list3[1:])
+
+# 4) create dictionary and assign to var
+week = {
+    "sunday":1,
+    "monday":2,
+    "tuesday":3,
+    "wednesday":4,
+    "thursday":5,
+    "friday":6,
+    "saturday":7
+}
+
+day = week["sunday"]
+print("Sunday is day:", day)
