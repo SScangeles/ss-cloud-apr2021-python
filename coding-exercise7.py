b521d549836426e0e3de00c0e58c412e3c45dd95
christianangeles <christian.angeles@smoothstack.com>
2021-04-23 13:16:05 -0700

    init code exercise7

---
+++ coding-exercise7.py
@@ -0,0 +1,18 @@
+
+# 1) Find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included)
+div7_notMult5 = [num for num in range(1500, 2701) if num%7 == 0 and num%5 == 0]
+print(div7_notMult5)
+
+# 2) Convert celsius to and from fahrenheit
+
+# 3) Guess that number!
+
+# 4) Pattern
+
+# 5) Reverse input
+
+# 6) Count even and odd numbers
+
+# 7) Print mixed list datatypes
+
+# 8) Print 0-6 except 3 and 6
